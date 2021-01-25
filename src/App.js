7f52c6a2217ee2f2616dd5e8a245d9d25200ba7a
Yoda2171 <popotaya7@gmail.com>
2021-01-25 12:58:50 -0300

    monto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./component/Header"
+import Home from "./wiews/home";
+import NotFound from "./wiews/NotFound";
 function App() {
+
+  const [state, setState] = useState({
+    user: null,
+  });
+  
+  useEffect (() => {
+    getUser("https://api.github.com/users/Yoda2171")
+    return() => {
+
+    }
+  },[])
+  
+  
+  const getUser = async (url) => {
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json();
+
+      setState({
+        ...state,
+        user: data,
+      });
+    }catch(error){
+      console.error(error.message);
+    }
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header user={state.user}/>
+        <div className="main-wrapper">
+        <Switch>
+          <Route exact path ="/" component={Home}/> 
+          <Route component={NotFound} />
+        </Switch>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import App from './App';
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "./index.css";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
